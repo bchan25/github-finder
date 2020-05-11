78d5555828c25378f4f7455a47a2d20d73d417e3
Ben Chan <ben@chanhq.com>
2020-05-11 14:16:43 +0100

    Move Repos state to context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -9,7 +9,7 @@ import {
   GET_USER,
   GET_REPOS,
 } from "../types";
-import Repos from "../../components/repos/Repos";
+//import Repos from "../../components/repos/Repos";
 
 const GithubState = (props) => {
   const initialState = {
@@ -44,6 +44,14 @@ const GithubState = (props) => {
   };
 
   // Get Repos
+  const getUserRepos = async (username) => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({ type: GET_REPOS, payload: res.data });
+  };
 
   // Clear Users
   const clearUsers = () => dispatch({ type: CLEAR_USERS });
@@ -61,6 +69,7 @@ const GithubState = (props) => {
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos,
       }}
     >
       {props.children}
